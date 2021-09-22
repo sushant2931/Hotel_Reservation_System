c05ecf7cc345d30c40058128aff12f53441513fc
sushant2931 <sushantlad8888@gmail.com>
2021-09-22 14:03:03 +0530

    Added : Welcome Message On Master Branch

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("....... Welcome In Hotel Reservation System .....");
+    }
+}
